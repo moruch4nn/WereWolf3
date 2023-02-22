4d22a3e7dce061552ff738c481ed211aa7ef21c8
moruch4nn <moruch4nn@gmail.com>
2023-02-22 20:35:43 +0900

    🐛偽霊媒アイテムが霊媒アイテムと一致いていないバグを修正

--- src/main/kotlin/dev/mr3n/werewolf3/items/madman/FakeMediumItem.kt
+++ src/main/kotlin/dev/mr3n/werewolf3/items/madman/FakeMediumItem.kt
@@ -1,7 +1,7 @@
 package dev.mr3n.werewolf3.items.madman
 
 import dev.mr3n.werewolf3.items.IShopItem
-import dev.mr3n.werewolf3.items.seer.SeerItem
+import dev.mr3n.werewolf3.items.medium.MediumItem
 
 @Suppress("unused")
-object FakeMediumItem: IShopItem.ShopItem("fake_medium", SeerItem.material)
+object FakeMediumItem: IShopItem.ShopItem("fake_medium", MediumItem.material)

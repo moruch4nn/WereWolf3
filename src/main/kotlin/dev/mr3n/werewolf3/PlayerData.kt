8916e97a843e7d489b756c532fe5077d2e586efd
moruch4nn <moruch4nn@gmail.com>
2023-02-27 02:56:55 +0900

    🐛途中抜けした際に起きる様々なバグを修正

--- src/main/kotlin/dev/mr3n/werewolf3/PlayerData.kt
+++ src/main/kotlin/dev/mr3n/werewolf3/PlayerData.kt
@@ -1,8 +1,10 @@
 package dev.mr3n.werewolf3
 
 import dev.mr3n.werewolf3.roles.Role
+import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
 
 class PlayerData(player: OfflinePlayer) {
     val uniqueId = player.uniqueId
@@ -10,10 +12,11 @@ class PlayerData(player: OfflinePlayer) {
     var role: Role? = null
     var co: Role? = null
     var will: String? = null
-    val offlinePlayer: OfflinePlayer = player
+    val player: Player?
+        get() = Bukkit.getPlayer(uniqueId)
     val kills = mutableSetOf<PlayerData>()
     val isAlive: Boolean
-        get() = (offlinePlayer.player?.gameMode?:GameMode.SPECTATOR) != GameMode.SPECTATOR
+        get() = (player?.gameMode?:GameMode.SPECTATOR) != GameMode.SPECTATOR
 
     override fun equals(other: Any?): Boolean {
         return other is PlayerData && other.uniqueId == this.uniqueId

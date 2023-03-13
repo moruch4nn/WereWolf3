b88b375e90be7182b128dda95a33680b6ca48d55
moruch4nn <moruch4nn@gmail.com>
2023-03-13 17:37:47 +0900

    ⭐偽範囲占いを追加

---
+++ src/main/kotlin/dev/mr3n/werewolf3/items/madman/FakeMultipleDivinationItem.kt
@@ -0,0 +1,8 @@
+package dev.mr3n.werewolf3.items.madman
+
+import dev.mr3n.werewolf3.items.IShopItem
+import dev.mr3n.werewolf3.items.seer.DivinationItem
+import dev.mr3n.werewolf3.items.seer.MultipleDivinationItem
+
+@Suppress("unused")
+object FakeMultipleDivinationItem: IShopItem.ShopItem("fake_multiple_divination", MultipleDivinationItem.material)

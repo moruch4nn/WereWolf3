64874be71ee1ad335226d04f4c75d98a9535b7dd
moruch4nn <moruch4nn@gmail.com>
2023-03-16 19:56:04 +0900

    ⭐️自爆アイテムを使用した際に音とパーティクルを追加

--- src/main/kotlin/dev/mr3n/werewolf3/items/wolf/SelfBomb.kt
+++ src/main/kotlin/dev/mr3n/werewolf3/items/wolf/SelfBomb.kt
@@ -6,6 +6,8 @@ import dev.mr3n.werewolf3.items.IShopItem
 import dev.mr3n.werewolf3.utils.alivePlayers
 import dev.mr3n.werewolf3.utils.damageTo
 import org.bukkit.Material
+import org.bukkit.Particle
+import org.bukkit.Sound
 import org.bukkit.event.player.PlayerInteractEvent
 
 @Suppress("unused")
@@ -17,6 +19,8 @@ object SelfBomb: IShopItem.ShopItem("self_bomb", Material.TNT) {
             val player = event.player
             val item = event.item
             if(item == null || !isSimilar(item)) { return@registerEvent }
+            player.world.playSound(player.location, Sound.ENTITY_GENERIC_EXPLODE, 2f, 1f)
+            player.world.spawnParticle(Particle.EXPLOSION_HUGE,player.location, 100)
             alivePlayers().filter { it.location.distance(player.location) < DISTANCE }
                 .sortedBy { it.location.distance(player.location) }
                 .forEach { player1 -> player.damageTo(player1, -1.0) }
